b6d9407e6b8070227ac0bf2989b8cebff39d493a
Md Emon Chowdhury <emonc.dev@gmail.com>
2026-09-24 10:40:25 +0600

    feat: added login functionality using shadcn

---
+++ app/login/page.tsx
@@ -0,0 +1,84 @@
+'use client'
+import { auth, loginUser } from "@/lib/auth";
+import { FormEvent, useState } from "react"
+import { toast } from "sonner";
+import { Button } from "@/components/ui/button"
+import { Card, CardContent, CardDescription, CardHeader, CardTitle, } from "@/components/ui/card"
+import { Field, FieldDescription, FieldGroup, FieldLabel, } from "@/components/ui/field"
+import { Input } from "@/components/ui/input"
+import Link from "next/link";
+
+export default function LoginPage() {
+    const [email, setEmail] = useState<string>("");
+    const [password, setPassword] = useState<string>("");
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+
+    const handleUserLogin = async (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        setIsLoading(true);
+        if (email.trim() && password.trim()) {
+            try {
+                const user = await loginUser(email, password);
+                console.log(user.user);
+                console.log("CURRENT USER: ", auth.currentUser)
+                toast.success("Logged in Successfully");
+
+            } catch (error) {
+                console.error(error)
+                toast.error("User Login Failed");
+
+            }
+            finally {
+                setIsLoading(false);
+            }
+        }
+    }
+
+    return (
+        <div className="border grid place-items-center">
+
+            <Card className="min-w-sm">
+                <CardHeader>
+                    <CardTitle>Login to your account</CardTitle>
+                    <CardDescription>
+                        Enter your email below to login to your account
+                    </CardDescription>
+                </CardHeader>
+                <CardContent>
+                    <form onSubmit={handleUserLogin}>
+                        <FieldGroup>
+                            <Field>
+                                <FieldLabel htmlFor="email">Email</FieldLabel>
+                                <Input
+                                    id="email"
+                                    type="email"
+                                    value={email}
+                                    placeholder="m@example.com"
+                                    required
+                                    onChange={(e) => setEmail(e.target.value)}
+                                />
+                            </Field>
+                            <Field>
+                                <FieldLabel htmlFor="password">Password</FieldLabel>
+                                <Input
+                                    id="password"
+                                    type="password"
+                                    required
+                                    onChange={(e) => setPassword(e.target.value)} />
+                            </Field>
+                            <Field>
+                                <Button type="submit" disabled={isLoading}>Login</Button>
+
+                                <FieldDescription className="text-center">
+                                    Don't have an account? <Link href={"/register"}>Sign up</Link>
+                                </FieldDescription>
+                            </Field>
+                        </FieldGroup>
+                    </form>
+                </CardContent>
+            </Card>
+
+
+        </div>
+    )
+}
--- lib/auth.ts
+++ lib/auth.ts
@@ -1,9 +1,12 @@
 import { app } from "@/lib/firebase";
-import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
 
-const auth = getAuth(app);
-console.log("AUTH FILE EXECUTED")
+export const auth = getAuth(app);
 
 export const createUser = (email: string, password: string) => {
     return createUserWithEmailAndPassword(auth, email, password);
+}
+
+export const loginUser = (email: string, password: string) => {
+    return signInWithEmailAndPassword(auth, email, password)
 }
